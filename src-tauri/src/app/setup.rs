50e46f4064bce85021ac78e1ac42fe40ad6ca437
lencx <cxin1314@gmail.com>
2022-12-07 22:12:05 +0800

    fix: build error

--- src-tauri/src/app/setup.rs
+++ src-tauri/src/app/setup.rs
@@ -27,7 +27,7 @@ pub fn init(app: &mut App) -> std::result::Result<(), Box<dyn std::error::Error>
         .initialization_script(include_str!("../core.js"))
         .initialization_script(&utils::user_script())
         .inner_size(800.0, 600.0)
-        .hidden_title(true)
+        .title("ChatGPT")
         .user_agent("5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
         .build()?;
 
