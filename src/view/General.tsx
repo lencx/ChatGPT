9a392a71f64bc620a8f2956e2185b9a2d74b22d2
lencx <cxin1314@gmail.com>
2022-12-31 12:43:47 +0800

    chore: conf path

--- src/view/General.tsx
+++ src/view/General.tsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from 'react';
 import { Form, Radio, Switch, Input, Button, Space, message, Tooltip } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
-import { invoke } from '@tauri-apps/api';
+import { invoke, shell, path } from '@tauri-apps/api';
 import { platform } from '@tauri-apps/api/os';
 import { ask } from '@tauri-apps/api/dialog';
 import { relaunch } from '@tauri-apps/api/process';
 import { clone, omit, isEqual } from 'lodash';
 
-import { DISABLE_AUTO_COMPLETE } from '@/utils';
+import useInit from '@/hooks/useInit';
+import { DISABLE_AUTO_COMPLETE, chatRoot } from '@/utils';
 
 const OriginLabel = ({ url }: { url: string }) => {
   return (
     <span>
-      Switch Origin <Tooltip title={`Default: ${url}`}><QuestionCircleOutlined /></Tooltip>
+      Switch Origin <Tooltip title={`Default: ${url}`}><QuestionCircleOutlined style={{ color: '#1677ff' }} /></Tooltip>
     </span>
   )
 }
@@ -29,7 +30,7 @@ const GlobalShortcut = () => {
           <a href="https://tauri.app/v1/api/js/globalshortcut" target="_blank">https://tauri.app/v1/api/js/globalshortcut</a>
         </div>
       )}>
-        <QuestionCircleOutlined />
+        <QuestionCircleOutlined style={{ color: '#1677ff' }} />
       </Tooltip>
     </div>
   )
@@ -37,18 +38,17 @@ const GlobalShortcut = () => {
 
 export default function General() {
   const [form] = Form.useForm();
+  const [jsonPath, setJsonPath] = useState('');
   const [platformInfo, setPlatform] = useState<string>('');
   const [chatConf, setChatConf] = useState<any>(null);
 
-  const init = async () => {
+  useInit(async () => {
+    setJsonPath(await path.join(await chatRoot(), 'chat.conf.json'));
+
     setPlatform(await platform());
     const chatData = await invoke('get_chat_conf');
     setChatConf(chatData);
-  }
-
-  useEffect(() => {
-    init();
-  }, [])
+  });
 
   useEffect(() => {
     form.setFieldsValue(clone(chatConf));
@@ -73,47 +73,54 @@ export default function General() {
   };
 
   return (
-    <Form
-      form={form}
-      style={{ maxWidth: 500 }}
-      onFinish={onFinish}
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 15, offset: 1 }}
-    >
-      <Form.Item label="Theme" name="theme">
-        <Radio.Group>
-          <Radio value="Light">Light</Radio>
-          <Radio value="Dark">Dark</Radio>
-        </Radio.Group>
-      </Form.Item>
-      <Form.Item label="Stay On Top" name="stay_on_top" valuePropName="checked">
-        <Switch />
-      </Form.Item>
-      <Form.Item label={<GlobalShortcut />} name="global_shortcut">
-        <Input placeholder="CmdOrCtrl+Shift+O" {...DISABLE_AUTO_COMPLETE} />
-      </Form.Item>
-      {platformInfo === 'darwin' && (
-        <Form.Item label="Titlebar" name="titlebar" valuePropName="checked">
+    <>
+      <div className="chat-table-tip">
+        <div className="chat-sync-path">
+          <div>PATH: <a onClick={() => shell.open(jsonPath)} title={jsonPath}>{jsonPath}</a></div>
+        </div>
+      </div>
+      <Form
+        form={form}
+        style={{ maxWidth: 500 }}
+        onFinish={onFinish}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 15, offset: 1 }}
+      >
+        <Form.Item label="Theme" name="theme">
+          <Radio.Group>
+            <Radio value="Light">Light</Radio>
+            <Radio value="Dark">Dark</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item label="Stay On Top" name="stay_on_top" valuePropName="checked">
           <Switch />
         </Form.Item>
-      )}
-      <Form.Item label={<OriginLabel url={chatConf?.default_origin} />} name="origin">
-        <Input placeholder="https://chat.openai.com" {...DISABLE_AUTO_COMPLETE} />
-      </Form.Item>
-      <Form.Item label="User Agent (Window)" name="ua_window">
-        <Input.TextArea autoSize={{ minRows: 4, maxRows: 4 }} {...DISABLE_AUTO_COMPLETE} placeholder="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" />
-      </Form.Item>
-      <Form.Item label="User Agent (SystemTray)" name="ua_tray">
-        <Input.TextArea autoSize={{ minRows: 4, maxRows: 4 }} {...DISABLE_AUTO_COMPLETE} placeholder="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" />
-      </Form.Item>
-      <Form.Item>
-        <Space size={20}>
-          <Button onClick={onCancel}>Cancel</Button>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Space>
-      </Form.Item>
-    </Form>
+        <Form.Item label={<GlobalShortcut />} name="global_shortcut">
+          <Input placeholder="CmdOrCtrl+Shift+O" {...DISABLE_AUTO_COMPLETE} />
+        </Form.Item>
+        {platformInfo === 'darwin' && (
+          <Form.Item label="Titlebar" name="titlebar" valuePropName="checked">
+            <Switch />
+          </Form.Item>
+        )}
+        <Form.Item label={<OriginLabel url={chatConf?.default_origin} />} name="origin">
+          <Input placeholder="https://chat.openai.com" {...DISABLE_AUTO_COMPLETE} />
+        </Form.Item>
+        <Form.Item label="User Agent (Window)" name="ua_window">
+          <Input.TextArea autoSize={{ minRows: 4, maxRows: 4 }} {...DISABLE_AUTO_COMPLETE} placeholder="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" />
+        </Form.Item>
+        <Form.Item label="User Agent (SystemTray)" name="ua_tray">
+          <Input.TextArea autoSize={{ minRows: 4, maxRows: 4 }} {...DISABLE_AUTO_COMPLETE} placeholder="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" />
+        </Form.Item>
+        <Form.Item>
+          <Space size={20}>
+            <Button onClick={onCancel}>Cancel</Button>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Space>
+        </Form.Item>
+      </Form>
+    </>
   )
 }
